96d8d899dc660cb57f821201f48ccf632335ef3a
abhinay100 <abhinay212@gmail.com>
2015-07-27 00:18:22 -0700

    changed password as password replacing admin

--- wp-config.php
+++ wp-config.php
@@ -22,7 +22,7 @@ define('DB_NAME', 'ubu_wp');
 define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'admin');
+define('DB_PASSWORD', 'password');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
